e8f0bbc2433c782a2ad4e7e46896b87830aef45e
Fernanda Rizky Prabowo <89926535+FernandaRizky27@users.noreply.github.com>
2022-08-29 22:33:50 +0700

    Add files via upload

---
+++ MyProject1/src/myproject1/MyMainClass.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package myproject1;
+
+/**
+ *
+ * @author Fernanda R
+ */
+public class MyMainClass {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.print("Hello World");
+        System.out.println("Hello Java");
+        System.out.println("java is fun");
+    }
+    
+}
---
+++ MyProject2/src/myproject2/MyNameProject.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package myproject2;
+
+/**
+ *
+ * @author Fernanda R
+ */
+public class MyNameProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("FFFFFFFFF     EEEEEEEEE    RRRRRRR");
+        System.out.println("FFFFFFFFF     EEEEEEEEE    RRRRRRRR");
+        System.out.println("FFF           EEE          RRR   RRR");
+        System.out.println("FFF           EEE          RRR    RR");
+        System.out.println("FFFFFFFFF     EEEEEEE      RRR   RRR");
+        System.out.println("FFFFFFFFF     EEEEEEE      RRRRRRRR");
+        System.out.println("FFF           EEE          RRRRRRR");
+        System.out.println("FFF           EEE          RRR RRR  ");
+        System.out.println("FFF           EEEEEEEE     RRR  RRR ");
+        System.out.println("FFF           EEEEEEEE     RRR   RRR");
+    }
+    
+}
